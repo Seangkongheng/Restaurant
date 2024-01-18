2c15e8e7ae992b4d5c5fa863bf49aeb2618cbc73
Seang kong heng <121208958+Seangkongheng@users.noreply.github.com>
2024-01-18 20:48:44 +0700

    fistcommit

---
+++ admin/index.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../admin//style/Admin.css">
+</head>
+<body>
+    <!-- menu section start -->
+    <div class="menu">
+        <div class="nav__admin">
+            <ul class="navIem">
+                <li class="nav__link"><a href="">Home</a></li>
+                <li class="nav__link"><a href="">Admin</a></li>
+                <li class="nav__link"><a href="">Category</a></li>
+                <li class="nav__link"><a href="">Food</a></li>
+                <li class="nav__link"><a href="">Order</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- menu section end -->
+
+    <!-- main content section start -->
+    <div class="main">
+         <div class="container">
+         Main here..
+         </div>
+    </div>
+    <!-- main content section end -->
+
+    <!-- footer section start -->
+   <footer class="bg-black">
+        <p class="text-center text-white">2023 copy by seang kong heng</p>
+   </footer>
+    <!-- footer section end -->
+</body>
+</html>
